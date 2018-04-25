754f76583ee332dffb62f242e77d6e441c000748
Wojciech Rydzewski <woj.rydzewski@gmail.com>
2018-04-25 23:05:37 +0100

    improved renaming when exif data was null

--- src/pl/wrryy/IphoneRename.java
+++ src/pl/wrryy/IphoneRename.java
@@ -8,13 +8,24 @@ import com.drew.metadata.exif.ExifSubIFDDirectory;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class IphoneRename {
 
     static final String DATE_FORMAT = "yyyy-MM-dd HHmmss";
 
+    /**
+     * Loops through the folders
+     *
+     * @param folder
+     */
     static void readFolder(File folder) {
         for (File file : folder.listFiles()) {
             if (file.isDirectory()) {
@@ -25,33 +36,51 @@ public class IphoneRename {
         }
     }
 
-    static void rename(File file) {
+    /**
+     * Changes file's name to contain its creation datetime
+     *
+     * @param file
+     */
+    private static void rename(File file) {
         String fileExt = getFileExt(file);
-        if (!fileExt.equals("db")) {
-            StringBuilder sb = new StringBuilder();
-            String pathName = sb.append(file.getParent()).append("\\").append(getFileDate(file)).append(".").append(getFileExt(file)).toString();
+        if (!"db".equals(fileExt) && !"gif".equals(fileExt)) {
+            String pathName = buildNewPath(file);
             File temp = new File(pathName);
-            file.renameTo(temp);
+            boolean renamed = file.renameTo(temp);
+            if(!renamed){
+                System.out.println(file.getName());
+//                rename(file);
+            }
         }
     }
 
-    static String getFileExt(File file) {
+    /**
+     * Parses file's name for extension
+     *
+     * @param file
+     * @return file's extension
+     */
+    private static String getFileExt(File file) {
         String n = null;
         try {
-            n = file.getName().split("\\.")[1];
+            n = file.getName().split(".*\\.")[1];
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Failed parsing file extension :: " + file.getName());
         }
         return n;
     }
 
-    static String getFileDate(File file) {
-        String sdf = null;
+    /**
+     * @param file
+     * @return file creation's date
+     */
+   private static String getFileDate(File file) {
+        String dateString = null;
         try {
             Metadata metadata = ImageMetadataReader.readMetadata(file);
             ExifSubIFDDirectory directory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
             Date date = directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
-            sdf = new SimpleDateFormat(DATE_FORMAT).format(date);
+            dateString = new SimpleDateFormat(DATE_FORMAT).format(date);
         } catch (ImageProcessingException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -61,7 +90,54 @@ public class IphoneRename {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return sdf;
+        if (dateString == null) {
+            dateString = parseFileForData(file);
+        }
+        return dateString;
+    }
+
+   private static String parseFileForData(File file) {
+       System.out.println(file.getName());
+        String dateString = null;
+        long size = file.getTotalSpace();
+        try (RandomAccessFile accessFile = new RandomAccessFile(file, "r")) {
+            MappedByteBuffer buffer = accessFile.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, file.length());
+            int c = 0;
+            byte[] s = new byte[20];
+            Pattern pattern = Pattern.compile("(\\d{4}:\\d{2}:\\d{2} \\d{2}:\\d{2}:\\d{2})");
+            try{
+
+            for (int i = 0; i < file.getTotalSpace()-2; i++) {
+
+                s[c] = buffer.get(i);
+                c++;
+                if (c == 20) {
+                    String part = new String(s, Charset.forName("UTF-8"));
+                    Matcher matcher = pattern.matcher(part);
+                    if (matcher.find()) {
+                        System.out.println(matcher.group(1));
+                        return matcher.group(1);
+                    }
+                    s = new byte[20];
+                    c = 0;
+                }
+            }
+            }catch (IndexOutOfBoundsException e) {
+                e.printStackTrace();
+            }} catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dateString;
+    }
+
+    private static String buildNewPath(File file) {
+        StringBuilder sb = new StringBuilder();
+        return sb.append(file.getParent())
+                .append("\\")
+                .append(getFileDate(file))
+                .append(".")
+                .append(getFileExt(file))
+                .toString();
     }
 }
 
